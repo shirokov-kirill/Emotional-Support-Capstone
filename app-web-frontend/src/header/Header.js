24596ab63d716045797a693797c5877a775fc34e
Kirill Shirokov <ksshirokov01@gmail.com>
2024-03-20 18:13:39 +0100

    Add simple header

---
+++ app-web-frontend/src/header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css'; // Import the CSS file
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><a href="#">TODO</a></li>
+          <li><a href="#">Assessment</a></li>
+          <li><a href="#">TODO</a></li>
+        </ul>
+      </nav>
+      <h1>Emotional support</h1>
+    </header>
+  );
+};
+
+export default Header;
--- app-web-frontend/src/index.js
+++ app-web-frontend/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import EmotionAssessmentScreen from './Emotion assessment/EmotionAssessmentScreen';
+import Header from './header/Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Header/>
     <EmotionAssessmentScreen/>
   </React.StrictMode>
 );
