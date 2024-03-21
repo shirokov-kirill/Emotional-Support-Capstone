2e683e27d93f97d1bef1438d5e35ddc2da585426
J3ne4ek <5656tetetre@gmail.com>
2024-03-21 20:10:55 +0100

    add checking

--- app-web-frontend/src/App.js
+++ app-web-frontend/src/App.js
@@ -1,17 +1,48 @@
 import React, { useState } from 'react';
 import './App.css';
 
+function Footer() {
+    return (
+        <footer className="App-footer">
+            <p>© 2024 Emotional Support</p>
+        </footer>
+    );
+}
+
 function App() {
     const [isLogin, setIsLogin] = useState(true);
     const [password, setPassword] = useState('');
     const [confirmationPassword, setConfirmationPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [dob, setDob] = useState('');
+    const [name, setName] = useState('');
+    const [surname, setSurname] = useState('');
+    const [username, setUsername] = useState('');
+
+    const isFormEmpty = () => {
+        return !name || !surname || !dob || !email || !username || !password || !confirmationPassword;
+    }
+
+    const validateEmail = () => {
+        const re = /^[^@\s]+@[^@\s]+\.[^@\s]+$/;
+        return re.test(email.toLowerCase()) || email.length === 0;
+    }
 
     const isPasswordValid = () => {
         return password.length >= 8 || password.length === 0;
     }
 
-    const isPasswordSame = () => {
-        return password === confirmationPassword;
+    const isPasswordSame= () => {
+        return password === confirmationPassword || confirmationPassword.length === 0;
+    }
+
+    const isDOBValid = () => {
+        const age = new Date().getFullYear() - new Date(dob).getFullYear();
+        return age >= 13 || dob.length === 0;
+    }
+
+    const isFormValid = () => {
+        return isPasswordValid() && isPasswordSame() && validateEmail() && isDOBValid() && !isFormEmpty();
     }
 
     return (
@@ -32,31 +63,47 @@ function App() {
                     <form>
                         <input type="text" placeholder="Name" />
                         <input type="text" placeholder="Surname" />
-                        <input type="date" placeholder="Date of Birth" />
-                        <input type="email" placeholder="Email" />
+                        <input
+                            type="date"
+                            placeholder="Date of Birth"
+                            onChange={e => setDob(e.target.value)}
+                            style={isDOBValid() ? {} : {border: '1px solid lightcoral'}}
+                        />
+                        {!isDOBValid() && <p className="warning-message">You must be at least 13 years old to register.</p>}
+                        <input
+                            type="email"
+                            placeholder="Email"
+                            onChange={e => setEmail(e.target.value)}
+                            style={validateEmail() ? {} : {border: '1px solid lightcoral'}}
+
+                        />
+                        {!validateEmail() && <p className="warning-message">Please enter a valid Email.</p>}
                         <input type="text" placeholder="Username" />
                         <input
                             type="password"
                             placeholder="Password"
                             onChange={e => setPassword(e.target.value)}
+                            style={isPasswordValid() ? {} : {border: '1px solid lightcoral'}}
                         />
 
-                        {!isPasswordValid() && <p>Password must be at least 8 characters long.</p>}
+                        {!isPasswordValid() && <p className="warning-message">Password must be at least 8 characters long.</p>}
 
                         <input
                             type="password"
                             placeholder="Confirm Password"
                             onChange={e => setConfirmationPassword(e.target.value)}
+                            style={isPasswordSame() ? {} : {border: '1px solid lightcoral'}}
                         />
 
-                        {!isPasswordSame() && <p>Passwords must match the confirmation password.</p>}
+                        {!isPasswordSame() && <p className="warning-message">Passwords must match the confirmation password.</p>}
 
-                        <button type="submit" disabled={!isPasswordValid() || !isPasswordSame()}>Sign Up</button>
+                        <button type="submit" disabled={!isFormValid()}>Sign Up</button>
 
                     </form>
                     <button className="switch-form-button" onClick={() => setIsLogin(true)}>Back to Login</button>
                 </div>
             )}
+            <Footer />
         </div>
     );
 }
