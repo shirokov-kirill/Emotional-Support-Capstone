b0153107815a1435cec322f6491af92480f959f5
J3ne4ek <5656tetetre@gmail.com>
2024-03-21 19:27:26 +0100

    Add info in login page

--- app-web-frontend/src/App.js
+++ app-web-frontend/src/App.js
@@ -3,6 +3,16 @@ import './App.css';
 
 function App() {
     const [isLogin, setIsLogin] = useState(true);
+    const [password, setPassword] = useState('');
+    const [confirmationPassword, setConfirmationPassword] = useState('');
+
+    const isPasswordValid = () => {
+        return password.length >= 8 || password.length === 0;
+    }
+
+    const isPasswordSame = () => {
+        return password === confirmationPassword;
+    }
 
     return (
         <div className="App">
@@ -14,17 +24,37 @@ function App() {
                         <input type="password" placeholder="Password" />
                         <button type="submit">Login</button>
                     </form>
-                    <button className="switch-form-button" onClick={() => setIsLogin(false)}>Switch to Sign Up</button>
+                    <button className="switch-form-button" onClick={() => setIsLogin(false)}>Don't have an account? Sign Up!</button>
                 </div>
             ) : (
                 <div className="form-container">
                     <h2>Sign Up</h2>
                     <form>
+                        <input type="text" placeholder="Name" />
+                        <input type="text" placeholder="Surname" />
+                        <input type="date" placeholder="Date of Birth" />
+                        <input type="email" placeholder="Email" />
                         <input type="text" placeholder="Username" />
-                        <input type="password" placeholder="Password" />
-                        <button type="submit">Sign Up</button>
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            onChange={e => setPassword(e.target.value)}
+                        />
+
+                        {!isPasswordValid() && <p>Password must be at least 8 characters long.</p>}
+
+                        <input
+                            type="password"
+                            placeholder="Confirm Password"
+                            onChange={e => setConfirmationPassword(e.target.value)}
+                        />
+
+                        {!isPasswordSame() && <p>Passwords must match the confirmation password.</p>}
+
+                        <button type="submit" disabled={!isPasswordValid() || !isPasswordSame()}>Sign Up</button>
+
                     </form>
-                    <button className="switch-form-button" onClick={() => setIsLogin(true)}>Switch to Login</button>
+                    <button className="switch-form-button" onClick={() => setIsLogin(true)}>Back to Login</button>
                 </div>
             )}
         </div>
