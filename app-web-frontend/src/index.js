7249b79f012e27be72379dfb92458ba5c2bdb258
georgiizorabov <gosha.zorabov@gmail.com>
2024-03-21 21:53:42 +0100

    fix: restored App files and added Login to `/login`

--- app-web-frontend/src/index.js
+++ app-web-frontend/src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
+import App from './App';
 import Login from './Login';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Login />
-  </React.StrictMode>
+ReactDOM.render(
+    <React.StrictMode>
+        <Router>
+            <Routes>
+                <Route path="/" element={<App/>}/>
+                <Route path="/login"  element={<Login/>}/>
+            </Routes>
+        </Router>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
