fecc629b5c61528b38cf4bb389847a1665864f9e
antoniy.belishev <antoniy.belishev@gmail.com>
2024-03-21 18:40:34 +0100

    modified getMood

--- app-web-frontend/src/reusables/Mood/GetMood.js
+++ app-web-frontend/src/reusables/Mood/GetMood.js
@@ -1,9 +1,19 @@
-function getMood(day) {
-    return {
+function getMoodsForTimeFrame(startDate, endDate) {
+    const moods = [];
+  
+    let currentDate = new Date(startDate);
+    while (currentDate <= endDate) {
+      moods.push({
+        date: new Date(currentDate),
         color: "yellow",
-        smiley: "😊"
-    };
-}
+        smiley: "😐",
+      });
+  
+      currentDate.setDate(currentDate.getDate() + 1);
+    }
+  
+    return moods;
+  }
 
 
 export default getMood;
