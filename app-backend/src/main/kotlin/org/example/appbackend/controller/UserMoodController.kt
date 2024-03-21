f124477840833f37a9ab133710a1641e794666f2
Kirill Shirokov <ksshirokov01@gmail.com>
2024-03-21 19:22:56 +0100

    Add fix

--- app-backend/src/main/kotlin/org/example/appbackend/controller/UserMoodController.kt
+++ app-backend/src/main/kotlin/org/example/appbackend/controller/UserMoodController.kt
@@ -5,6 +5,7 @@ import org.example.appbackend.dto.UpdateUserMoodDto
 import org.example.appbackend.dto.UserMoodDto
 import org.example.appbackend.service.UserMoodService
 import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@CrossOrigin(origins = ["http://localhost:3000"])
 class UserMoodController(
     private val userMoodService: UserMoodService,
 ) {
