298d7456b0dbdaffcea9998203807f8a4de1329d
JaneKirillova <grindevald666@gmail.com>
2024-03-22 01:39:48 +0100

    Add login page for health providers and page for choosing login option

---
+++ app-web-frontend/src/Home/HealthProviderHome.js
@@ -0,0 +1,13 @@
+import Header from '../header/Header';
+
+
+function HealthProviderHome() {
+    return (
+        <div className="App">
+            <h2>Home Page for Health Provider</h2>
+        </div>
+    )
+}
+
+
+export default HealthProviderHome;
