7b0ccdb5402dc1d6ead6a3f4280b2efd1d686b66
Lakindu1999 <86758637+Lakindu1999@users.noreply.github.com>
2024-04-09 14:44:27 +0200

    added upcoming appoinment

--- app-web-frontend/src/App.js
+++ app-web-frontend/src/App.js
@@ -1,6 +1,13 @@
 import Profile from "./Components/ProfileComponent/Profile";
 import Feedback from "./Components/FeedbackComponent/Feedback";
 import Notification from "./Components/NotificationComponent/Notification";
+import DataTable from "./Components/DashboardComponent/UpcomingAppointment";
+
+const rows = [
+  { id: "ML23456", name: "John Appleseed", age: 25, status: "Confirmed" },
+  { id: "ML23457", name: "John Smith", age: 23, status: "No Info" },
+  // Add more rows as needed
+];
 
 function App() {
   return (
@@ -8,6 +15,8 @@ function App() {
       <Profile />
       <Feedback />
       <Notification />
+      <DataTable
+      rows = {rows}/>
     </div>
   );
 }
