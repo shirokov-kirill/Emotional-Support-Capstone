ea63623488ae199f523f8deebeb95b67090fad99
Mark Prudnikov <mark.prudnikov@jetbrains.com>
2024-04-09 22:27:39 +0200

    [G01 | Feedback] Introduce `FeedbackEntity`

---
+++ app-backend/src/main/kotlin/org/example/appbackend/entity/FeedbackEntity.kt
@@ -0,0 +1,56 @@
+package org.example.appbackend.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.SequenceGenerator
+import jakarta.persistence.Table
+import org.example.appbackend.dto.FeedbackDto
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "feedback")
+class FeedbackEntity() {
+
+    constructor(dto: FeedbackDto) : this() {
+        name = dto.name
+        email = dto.email
+        phone = dto.phone
+        companyName = dto.companyName
+        rating = dto.rating
+        comment = dto.comment
+    }
+
+    @Id
+    @GeneratedValue(generator = SEQUENCE_GENERATOR_NAME, strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = SEQUENCE_GENERATOR_NAME, sequenceName = SEQUENCE_NAME, allocationSize = 1)
+    var id: Int? = null
+
+    @Column(name = "created")
+    var created: LocalDateTime? = null
+
+    @Column(name = "name")
+    var name: String? = null
+
+    @Column(name = "email")
+    var email: String? = null
+
+    @Column(name = "phone")
+    var phone: String? = null
+
+    @Column(name = "company")
+    var companyName: String? = null
+
+    @Column(name = "rating")
+    var rating: Int? = null
+
+    @Column(name = "comment")
+    var comment: String? = null
+
+    companion object {
+        private const val SEQUENCE_GENERATOR_NAME = "feedback_id_sequence"
+        private const val SEQUENCE_NAME = "seq_feedback_id"
+    }
+}
