829ec1e29b4dd1d17b6de40af69137249f55f8d4
Mark Prudnikov <mark.prudnikov@jetbrains.com>
2024-04-09 22:30:54 +0200

    [G01 | Feedback] Post feedback endpoint

--- app-backend/src/main/kotlin/org/example/appbackend/controller/FeedbackController.kt
+++ app-backend/src/main/kotlin/org/example/appbackend/controller/FeedbackController.kt
@@ -1,6 +1,20 @@
 package org.example.appbackend.controller
 
+import org.example.appbackend.dto.FeedbackDto
+import org.example.appbackend.entity.FeedbackEntity
+import org.example.appbackend.repository.FeedbackRepository
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class FeedbackController
+class FeedbackController(
+    private val feedbackRepository: FeedbackRepository
+) {
+
+    @PostMapping("/feedback")
+    fun postFeedback(@RequestBody feedbackDto: FeedbackDto): FeedbackEntity {
+        val feedback = FeedbackEntity(feedbackDto)
+        return feedbackRepository.save(feedback)
+    }
+}
