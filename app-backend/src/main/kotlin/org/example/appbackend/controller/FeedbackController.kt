9ddaa0c157e04f870615d293845e869033aaedac
Mark Prudnikov <mark.prudnikov@jetbrains.com>
2024-04-09 22:31:20 +0200

    [G01 | Feedback] Get all feedback endpoint

--- app-backend/src/main/kotlin/org/example/appbackend/controller/FeedbackController.kt
+++ app-backend/src/main/kotlin/org/example/appbackend/controller/FeedbackController.kt
@@ -1,8 +1,10 @@
 package org.example.appbackend.controller
 
 import org.example.appbackend.dto.FeedbackDto
+import org.example.appbackend.dto.toDto
 import org.example.appbackend.entity.FeedbackEntity
 import org.example.appbackend.repository.FeedbackRepository
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -17,4 +19,7 @@ class FeedbackController(
         val feedback = FeedbackEntity(feedbackDto)
         return feedbackRepository.save(feedback)
     }
+
+    @GetMapping("/feedback")
+    fun getFeedback(): List<FeedbackDto> = feedbackRepository.findAll().map { it.toDto() }
 }
