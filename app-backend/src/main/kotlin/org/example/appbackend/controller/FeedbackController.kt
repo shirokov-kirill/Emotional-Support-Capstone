d12be5fa8ec9bc9ea86ffc768e9685e77abc735c
Mark Prudnikov <mark.prudnikov@jetbrains.com>
2024-04-11 21:55:59 +0200

    [G01 | Feedback] Add `@CrossOrigin` to `FeedbackController`
    Allows to run both backend and fronted application locally for the development purposes.

--- app-backend/src/main/kotlin/org/example/appbackend/controller/FeedbackController.kt
+++ app-backend/src/main/kotlin/org/example/appbackend/controller/FeedbackController.kt
@@ -4,12 +4,14 @@ import org.example.appbackend.dto.FeedbackDto
 import org.example.appbackend.entity.FeedbackEntity
 import org.example.appbackend.mapper.FeedbackEntityMapper
 import org.example.appbackend.repository.FeedbackRepository
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@CrossOrigin(origins = ["http://localhost:3000"], maxAge = 3600)
 class FeedbackController(
     private val feedbackRepository: FeedbackRepository,
     private val feedbackEntityMapper: FeedbackEntityMapper
