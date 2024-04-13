dfa77b6b59a1580ecdd68f261bcbbffc1e4f0cab
ros <vbatraev@gmail.com>
2024-04-13 21:28:05 +0300

    Fix indent.

--- app-backend/build.gradle.kts
+++ app-backend/build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("org.springframework.boot") version "3.2.3"
-  id("io.spring.dependency-management") version "1.1.4"
-  id("org.jetbrains.kotlin.kapt") version "1.9.22"
-  kotlin("jvm") version "1.9.22"
-  kotlin("plugin.spring") version "1.9.22"
+    id("org.springframework.boot") version "3.2.3"
+    id("io.spring.dependency-management") version "1.1.4"
+    id("org.jetbrains.kotlin.kapt") version "1.9.22"
+    kotlin("jvm") version "1.9.22"
+    kotlin("plugin.spring") version "1.9.22"
 }
 
 group = "org.example"
@@ -14,36 +14,36 @@ version = "0.0.1-SNAPSHOT"
 val mapstructVersion by extra("1.5.5.Final")
 
 java {
-  sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 repositories {
-  mavenCentral()
+    mavenCentral()
 }
 
 kapt {
-  correctErrorTypes= true
+    correctErrorTypes= true
 }
 
 dependencies {
-  implementation("org.springframework.boot:spring-boot-starter-web")
-  implementation("org.springframework.boot:spring-boot-starter-logging")
-  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-  implementation("org.springframework.boot:spring-boot-starter-security")
-  implementation("org.springframework.boot:spring-boot-starter-jdbc")
-  implementation("org.springframework.session:spring-session-jdbc")
-  implementation("org.springframework.security:spring-security-config")
-  implementation("org.jetbrains.kotlin:kotlin-reflect")
-  implementation("org.liquibase:liquibase-core:4.26.0")
-  implementation("org.postgresql:postgresql:42.7.3")
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-  implementation("io.jsonwebtoken:jjwt-api:0.11.2")
-  implementation("io.jsonwebtoken:jjwt-impl:0.11.2")
-  implementation("io.jsonwebtoken:jjwt-jackson:0.11.2")
-  implementation("org.mapstruct:mapstruct:$mapstructVersion")
-  kapt("org.mapstruct:mapstruct-processor:$mapstructVersion")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-logging")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("org.springframework.session:spring-session-jdbc")
+    implementation("org.springframework.security:spring-security-config")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.liquibase:liquibase-core:4.26.0")
+    implementation("org.postgresql:postgresql:42.7.3")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("io.jsonwebtoken:jjwt-api:0.11.2")
+    implementation("io.jsonwebtoken:jjwt-impl:0.11.2")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.11.2")
+    implementation("org.mapstruct:mapstruct:$mapstructVersion")
+    kapt("org.mapstruct:mapstruct-processor:$mapstructVersion")
 
-  runtimeOnly("com.h2database:h2")
+    runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("com.h2database:h2")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.9.22")
@@ -51,12 +51,12 @@ dependencies {
 }
 
 tasks.withType<KotlinCompile> {
-  kotlinOptions {
-    freeCompilerArgs += "-Xjsr305=strict"
-    jvmTarget = "17"
-  }
+    kotlinOptions {
+      freeCompilerArgs += "-Xjsr305=strict"
+      jvmTarget = "17"
+    }
 }
 
 tasks.withType<Test> {
-  useJUnitPlatform()
+    useJUnitPlatform()
 }
