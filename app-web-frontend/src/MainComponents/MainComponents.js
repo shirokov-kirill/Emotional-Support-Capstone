28e43b1d38d8e5d0772b1adacf303eeab34f6ca1
Aleksandra Sorokovikova <71643875+AleksandraSorokovikova@users.noreply.github.com>
2024-04-24 21:00:57 +0200

    fixed MainComponents (2)

--- app-web-frontend/src/MainComponents/MainComponents.js
+++ app-web-frontend/src/MainComponents/MainComponents.js
@@ -4,7 +4,6 @@ import Feedback from './Feedback/Feedback';
 import FeedbackList from './Feedback/FeedbackList';
 import Notification from "../Components/NotificationComponent/Notification";
 import HomeIcon from "@mui/icons-material/Cottage";
-import AssessmentIcon from "@mui/icons-material/Assessment";
 import CalendarIcon from "@mui/icons-material/CalendarMonth";
 import SmsIcon from "@mui/icons-material/Sms";
 import FeedbackIcon from "@mui/icons-material/Feedback";
@@ -17,18 +16,13 @@ import Dashboard from "../scenes/Dashboard";
 
 const MainComponents = [
     { "label": "Dashboard", "path": "/dashboard", "element": Dashboard, "icon": <HomeIcon /> },
-    { "label": "Assessment", "path": "/assessment", "element": EmotionAssessmentForm, icon: <AssessmentIcon /> },
     { "label": "Calendar", "path": "/calendar", "element": Calendar, icon: <CalendarIcon /> },
     { "label": "Chats", "path": "/chats", "element": ChatsPage, icon: <SmsIcon /> },
     { "label": "Appointments", "path": "/appointment", "element": Appointment, icon: <AppointmentIcon /> },
     { "label": "Feedback", "path": "/feedback", "element": Feedback, icon: <FeedbackIcon /> },
     { "label": "Feedback list", "path": "/feedbackList", "element": FeedbackList, icon: <FeedbackIcon /> },
     { "label": "Notifications list", "path": "/notifications", "element": Notification },
-    { "label": "Profile", "path": "/profile", "element": MainProfile, "icon": <ProfileIcon /> },
-    { "label": "Home", "path": "/home", "element": Home },
-    { "label": "Sharing Data", "path": "/sharing", "element": DataSharing},
-    {"label": "Viewing Data", "path": "/viewing", "element": DataViewing},
-    { "label": "Changes", "path": "/criticalChanges", "element": CriticalChanges }
+    { "label": "Profile", "path": "/profile", "element": MainProfile, "icon": <ProfileIcon /> }
 ]
 
 export default MainComponents;
