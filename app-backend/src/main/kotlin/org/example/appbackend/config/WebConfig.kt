b93bb548da2c565910dce0fa0c4e4da11e766db8
Evgeniia Kirillova <evgeniia.kirillova@jetbrains.com>
2024-10-30 21:27:55 +0100

    Fix problem with login for users

---
+++ app-backend/src/main/kotlin/org/example/appbackend/config/WebConfig.kt
@@ -0,0 +1,17 @@
+package org.example.appbackend.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+    }
+}
