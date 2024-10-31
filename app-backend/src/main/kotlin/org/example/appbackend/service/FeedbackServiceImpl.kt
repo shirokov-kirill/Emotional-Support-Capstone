d67282b5a195845914103051b8ec3f22e6f99f8b
Mark Prudnikov <mark.prudnikov@jetbrains.com>
2024-10-31 18:35:00 +0100

    [cleanup] Decouple `FeedbackController` from `FeedbackRepository`

---
+++ app-backend/src/main/kotlin/org/example/appbackend/service/FeedbackServiceImpl.kt
@@ -0,0 +1,22 @@
+package org.example.appbackend.service
+
+import org.example.appbackend.dto.FeedbackDto
+import org.example.appbackend.entity.FeedbackEntity
+import org.example.appbackend.mapper.FeedbackEntityMapper
+import org.example.appbackend.repository.FeedbackRepository
+import org.springframework.stereotype.Service
+
+@Service
+class FeedbackServiceImpl(
+    private val feedbackRepository: FeedbackRepository,
+    private val feedbackEntityMapper: FeedbackEntityMapper
+) : FeedbackService {
+    override fun saveFeedback(feedbackDto: FeedbackDto): FeedbackEntity {
+        val feedback = feedbackEntityMapper.dtoToEntity(feedbackDto)
+        return feedbackRepository.save(feedback)
+    }
+
+    override fun getAllFeedback(): List<FeedbackDto> {
+        return feedbackRepository.findAll().map { feedbackEntityMapper.entityToDto(it) }
+    }
+}
