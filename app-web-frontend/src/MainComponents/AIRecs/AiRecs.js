a049f7abe3d615578c965ea0e54fec4799850a1e
Evgeniia Kirillova <evgeniia.kirillova@jetbrains.com>
2024-11-15 00:51:03 +0100

    Add component with ai recommendations and different categories

---
+++ app-web-frontend/src/MainComponents/AIRecs/AiRecs.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './AiRecs.css';
+
+const ButtonGrid = () => {
+    const navigate = useNavigate();
+
+    const handleButtonClick = (category) => {
+        console.log(`You selected: ${category}`);
+        if (category === 'Breathing exercises') {
+            navigate('/breathing-exercises');
+        } else {
+            navigate(`/${category.toLowerCase().replace(" ", "-")}`);
+        }
+    };
+
+    return (
+        <div className="App">
+            <div className="content-container">
+                <h1>Choose about what would you like to ask AI:</h1>
+                <div className="button-container">
+                    <button className="grid-button" onClick={() => handleButtonClick('Breathing exercises')}>
+                        Breathing exercises
+                    </button>
+                    <button className="grid-button" onClick={() => handleButtonClick('Category 2')}>
+                        Category 2
+                    </button>
+                    <button className="grid-button" onClick={() => handleButtonClick('Category 3')}>
+                        Category 3
+                    </button>
+                    <button className="grid-button" onClick={() => handleButtonClick('Category 4')}>
+                        Category 4
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ButtonGrid;
