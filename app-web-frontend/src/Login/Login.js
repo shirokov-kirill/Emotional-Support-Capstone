9b77a7fc8e55bd4d4836eb1b12ab8f7069a6290f
Ekaterina Itsenko <ekaterinaicenko@gmail.com>
2024-11-19 23:03:52 +0100

    Differences with PR #139

--- app-web-frontend/src/Login/Login.js
+++ app-web-frontend/src/Login/Login.js
@@ -1,11 +1,9 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import './Login.css';
-import { useNavigate } from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 import axios from 'axios';
 import {SERVER_ADDRESS} from "../setupInfo";
 import PasswordInput from './Components/PasswordInput';
-
-
 
 
 function Footer() {
@@ -95,7 +93,7 @@ export function Login() {
         }
     }
 
-    const isPasswordSame= () => {
+    const isPasswordSame = () => {
         return password === confirmationPassword || confirmationPassword.length === 0;
     }
 
@@ -135,7 +133,7 @@ export function Login() {
         try {
             const response = await axios.post(SERVER_ADDRESS + '/auth/login', userLogin);
             if (response.status === 200) {
-		const authToken = response.data.token;
+                const authToken = response.data.token;
                 localStorage.setItem('authToken', authToken); // Save token to local storage
                 localStorage.setItem('role', 'patient')
                 console.log('User login successfully')
@@ -154,7 +152,7 @@ export function Login() {
             email,
             firstName,
             lastName,
-	        username,
+            username,
             dateOfBirth,
             gender,
             password,
@@ -221,164 +219,162 @@ export function Login() {
                     <button className="text-button back-to-role" onClick={() => navigate("/")}>
                         Back to role choice
                     </button>
-                            </div>
+                </div>
             ) : (
-            <div className="form-container signup" style={{ textAlign: "left" }}>
-                <h2>Sign Up</h2>
-                <form onSubmit={onNewUserFormSubmit}>
-                    <div className="horizontal-form">
-                        <div className="horizontal-column first-column">
-                            <input
-                                type="text"
-                                placeholder="Name"
-                                onChange={(e) => setFirstName(e.target.value)}
-                            />
-                        </div>
-                        <div className="horizontal-column">
-                            <input
-                                type="text"
-                                placeholder="Surname"
-                                onChange={(e) => setLastName(e.target.value)}
-                            />
-                        </div>
-                    </div>
-                    <div className="horizontal-form">
-                        <div className="horizontal-column first-column">
-                            <input
-                                type="text"
-                                placeholder="Username"
-                                onChange={(e) => setUsername(e.target.value)}
-                                />
-                                    {!isUsernameValid() && (
-                                        <p className="warning-message">
-                                            Has to be 5+ symbols long, no special characters
-                                        </p>
-                                    )}
-                                </div>
-                                <div className="horizontal-column">
+                <div className="form-container signup" style={{textAlign: "left"}}>
+                    <h2>Sign Up</h2>
+                    <form onSubmit={onNewUserFormSubmit}>
+                        <div className="horizontal-form">
+                            <div className="horizontal-column first-column">
                                 <input
-                                type="date"
-                                placeholder="1990-01-01"
-                                onChange={(e) => setDob(e.target.value)}
-                                style={isDOBValid() ? {} : {border: "1px solid lightcoral"}}
-                                className="custom-date-picker"
-                            />
-                            {!isDOBValid() && (
-                                <p className="warning-message">
-                                    Must be 13+ to register
-                                </p>
-                            )}
+                                    type="text"
+                                    placeholder="Name"
+                                    onChange={(e) => setFirstName(e.target.value)}
+                                />
+                            </div>
+                            <div className="horizontal-column">
+                                <input
+                                    type="text"
+                                    placeholder="Surname"
+                                    onChange={(e) => setLastName(e.target.value)}
+                                />
+                            </div>
                         </div>
-                    </div>
-                    <form class="gender-select">
-                        <label class="gender-option">
-                            <input type="radio" name="gender" value="male" onChange={(e) => setGender(e.target.value)}/>
-                            <span class="checkmark"></span> Male
-                        </label>
-                        <label class="gender-option">
-                            <input type="radio" name="gender" value="female"
-                                   onChange={(e) => setGender(e.target.value)}/>
-                            <span class="checkmark"></span> Female
-                        </label>
-                        <label class="gender-option">
-                            <input type="radio" name="gender" value="other"
-                                   onChange={(e) => setGender(e.target.value)}/>
-                            <span class="checkmark"></span> Other
-                        </label>
-                    </form>
+                        <div className="horizontal-form">
+                            <div className="horizontal-column first-column">
+                                <input
+                                    type="text"
+                                    placeholder="Username"
+                                    onChange={(e) => setUsername(e.target.value)}
+                                />
+                                {!isUsernameValid() && (
+                                    <p className="warning-message">
+                                        Has to be 5+ symbols long, no special characters
+                                    </p>
+                                )}
+                            </div>
+                            <div className="horizontal-column">
+                                <input
+                                    type="date"
+                                    placeholder="1990-01-01"
+                                    onChange={(e) => setDob(e.target.value)}
+                                    style={isDOBValid() ? {} : {border: "1px solid lightcoral"}}
+                                    className="custom-date-picker"
+                                />
+                                {!isDOBValid() && (
+                                    <p className="warning-message">
+                                        Must be 13+ to register
+                                    </p>
+                                )}
+                            </div>
+                        </div>
+                        <form class="gender-select">
+                            <label class="gender-option">
+                                <input type="radio" name="gender" value="male"
+                                       onChange={(e) => setGender(e.target.value)}/>
+                                <span class="checkmark"></span> Male
+                            </label>
+                            <label class="gender-option">
+                                <input type="radio" name="gender" value="female"
+                                       onChange={(e) => setGender(e.target.value)}/>
+                                <span class="checkmark"></span> Female
+                            </label>
+                            <label class="gender-option">
+                                <input type="radio" name="gender" value="other"
+                                       onChange={(e) => setGender(e.target.value)}/>
+                                <span class="checkmark"></span> Other
+                            </label>
+                        </form>
 
-                    <input
-                        type="email"
-                        placeholder="Email"
-                        onChange={(e) => setEmail(e.target.value)}
-                        style={validateEmail() ? {} : {border: "1px solid lightcoral"}}
-                    />
-                    {!validateEmail() && (
-                        <p className="warning-message">Please enter a valid Email.</p>
-                    )}
+                        <input
+                            type="email"
+                            placeholder="Email"
+                            onChange={(e) => setEmail(e.target.value)}
+                            style={validateEmail() ? {} : {border: "1px solid lightcoral"}}
+                        />
+                        {!validateEmail() && (
+                            <p className="warning-message">Please enter a valid Email.</p>
+                        )}
 
-                    <PasswordInput
+                        <PasswordInput
                             value={password}
                             placeholder={"Password"}
                             onChange={e => setPassword(e.target.value)}
                             isValid={isPasswordValid()}
                             showPassword={showPassword}
                             togglePasswordVisibility={togglePasswordVisibility}
-                    />
-                    {isPasswordValid() && password.length !== 0 && (() => {
-                    const strength = calculatePasswordStrength(password);
-                    return (
-                        <p className="warning-message" style={{ color: strength.color }}>
-                            {strength.message}
-                        </p>
-                    );
-                    })()}
-                    {!isPasswordValid() && (
-                        <p className="warning-message">
-                            Password must be at least 8 characters long.
-                        </p>
-                    )}
+                        />
+                        {isPasswordValid() && password.length !== 0 && (() => {
+                            const strength = calculatePasswordStrength(password);
+                            return (
+                                <p className="warning-message" style={{color: strength.color}}>
+                                    {strength.message}
+                                </p>
+                            );
+                        })()}
+                        {!isPasswordValid() && (
+                            <p className="warning-message">
+                                Password must be at least 8 characters long.
+                            </p>
+                        )}
 
-                    <PasswordInput
+                        <PasswordInput
                             value={confirmationPassword}
                             placeholder={"Confirm password"}
                             onChange={e => setConfirmationPassword(e.target.value)}
                             isValid={isPasswordSame()}
                             showPassword={showConfirmationPassword}
                             togglePasswordVisibility={toggleConfirmationPasswordVisibility}
-                    />
+                        />
 
-                    <select
-                        onChange={(e) => setSecurityQuestion(e.target.value)}
-                        value={securityQuestion}
-                        className="security-question-dropdown"
-                    >
-                        <option value="" disabled>Select a security question</option>
-                        {securityQuestions.map((question, index) => (
-                            <option key={index} value={question}>{question}</option>
-                        ))}
-                    </select>
+                        <select
+                            onChange={(e) => setSecurityQuestion(e.target.value)}
+                            value={securityQuestion}
+                            className="security-question-dropdown"
+                        >
+                            <option value="" disabled>Select a security question</option>
+                            {securityQuestions.map((question, index) => (
+                                <option key={index} value={question}>{question}</option>
+                            ))}
+                        </select>
 
-                    <input
-                        type="text"
-                        placeholder="Answer to selected security question"
-                        onChange={(e) => setSecurityAnswer(e.target.value)}
-                    />
+                        <input
+                            type="text"
+                            placeholder="Answer to selected security question"
+                            onChange={(e) => setSecurityAnswer(e.target.value)}
+                        />
 
-                    {!isPasswordSame() && (
-                        <p className="warning-message">
-                            Passwords must match the confirmation password.
-                        </p>
-                    )}
-
-                    <div>
-                        {showFormValidWarning && (
+                        {!isPasswordSame() && (
                             <p className="warning-message">
-                                Please fill in all the required fields.
+                                Passwords must match the confirmation password.
                             </p>
                         )}
-                        <button type="submit" disabled={!isNewUserFormValid()}>
-                            Sign Up
-                        </button>
-                    </div>
-                </form>
-                <button
-                    className="text-button signup-button"
-                    onClick={() => setIsLogin(true)}
-                >
-                    Back to Login
-                </button>
-            </div>
+
+                        <div>
+                            {showFormValidWarning && (
+                                <p className="warning-message">
+                                    Please fill in all the required fields.
+                                </p>
+                            )}
+                            <button type="submit" disabled={!isNewUserFormValid()}>
+                                Sign Up
+                            </button>
+                        </div>
+                    </form>
+                    <button
+                        className="text-button signup-button"
+                        onClick={() => setIsLogin(true)}
+                    >
+                        Back to Login
+                    </button>
+                </div>
             )}
             <Footer/>
         </div>
     );
 }
 
-
-// this logic isn't great, but this functiion is needed for the header to work
-// TODO: refactor this
-
-    export function isLoggedIn() {
-        return true;
-    }
+export function isLoggedIn() {
+    localStorage.getItem('authToken');
+    return localStorage.getItem('authToken') !== null;
+}
